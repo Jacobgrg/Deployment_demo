0011c985dfc93b18a8608fab7687fefe243833c5
Jacob Gurung <Jacobgurung@gmail.com>
2023-10-11 23:49:41 +0800

    Download update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './Components/pokemon.css'
 import "./Components/Modal.css";
 
 
-const csv_url="http://localhost:3000/Gurung,Jacob Riyoja.docx";
+const csv_url="https://idyllic-faloodeh-476f84.netlify.app/Gurung,Jacob Riyoja.docx";
 
 
 
@@ -45,38 +45,10 @@ function App() {
   
     closeModal &&
       closeModal.addEventListener("click", () => modal.close());
-  }
-
-  // const toggleModal = () =>{
-  //   setModal(!modal)
-  // }
-
-
-  // const handleKeyDown = event =>{
-  //   if (event.key === "Escape") {
-  //     setModal(!modal);
-  //   }
-  
-  // }
- 
- 
-
-  
+  }  
 
    
-
   
-
-
-
-
-
-
-    
-
-
-
-
 
   const particlesInit = async (main) => {
     console.log(main);
@@ -110,33 +82,12 @@ function App() {
                         </a>
                        </div>)}</Popup>
                         
-
-
-
-
-
-                      
-                      {/* <button id='openModal' className='button'>Education</button>
-                    <dialog id="modal">
-                      <div className='pkmn-text'>
-                        <h1>Hang Seng University of Hong Kong 香港恒生大學</h1>
-                        <a href="https://www.hsu.edu.hk/wp-content/uploads/2021/07/HSUHK-Pamphlet_Jul-2021_BA-AHCC_single.pdf" target="_blank"><p className='button'>Bachelor of Arts(Honours) in APPLIED AND HUMAN-CENTRED COMPUTING(BA-AHCC)</p></a>
-                        <h1>The HKU School of Professional and Continuing Education (HKU SPACE)</h1>
-                        <a href="https://www2.hkuspace.hku.hk/cc/higher-diploma/higher-diploma-in-data-science" target="_blank">
-                        <p className='button'>Higher Diploma in Data Science</p>
-                        </a>
-                        <p><button id="closeModal" className='button' ><b>Close</b></button></p>
-                       </div>
-                       </dialog>   */}
                     </p>
 
                     {/* This part is for the Resume section */}
                     <p ><button className='button' onClick={()=>{downloadFileURL(csv_url)}}>Resume</button></p>
 
                     {/* This part for Projects */}
-                    
-                    {/* <p ><button id='openModal' className='button'>Projects</button></p> */}
-                    
                     <p>
                     <Popup trigger=
                     {<button className='button'> Contacts </button>} modal nested>{
